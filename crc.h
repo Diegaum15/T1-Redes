2968e93c63d49c8751ef6ea31264d85d117702e6
Diego <109905655+Diegaum15@users.noreply.github.com>
2024-07-05 11:56:30 -0300

    Add files via upload
    Separei para deixar o cálculo do crc sozinho

---
+++ crc.h
@@ -0,0 +1,13 @@
+#ifndef CRC_H
+#define CRC_H
+
+// Função para calcular o CRC-8
+uint8_t calc_crc8_with_table(uint8_t *msg, int tamBytes);
+
+// Função para calcular o CRC-8
+uint8_t calc_crc8(protocolo *msg);
+
+// Função para calcular o CRC-8
+uint8_t gencrc(uint8_t *data, size_t len);
+
+#endif  // CRC_H
